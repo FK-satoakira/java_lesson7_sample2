29a03fbd26d8c21becf8f8300cb540528a4fc730
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-08-30 15:37:04 +0900

    Add files via upload

---
+++ smaple02/src/main/java/backingBeans/TopPage.java
@@ -0,0 +1,41 @@
+package backingBeans;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class TopPage implements Serializable{
+	private Integer number;
+	private String name="堀内";
+
+	public String next(){
+                System.out.println("number="+this.number+"/ name="+this.name);
+                test();
+		System.out.println("number="+this.number+"/ name="+this.name);
+//                test();
+
+//ここはpublic void test()も、public String test()も、可
+		return "output.xhtml";
+	}
+        public String test(){
+            this.name = "test";
+            return this.name;
+        }
+//        public void test(){
+//            this.name = "test";
+//        }
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public Integer getNumber(){
+		return number;
+	}
+	public void setNumber(Integer number){
+		this.number = number;
+	}
+}
